66dfb1f92f9a83686b0eeb8c4655b7290e5c8bd8
aabhushan <aabhushans62@gmail.com>
2026-08-11 15:30:50 +0545

    ProjectRegistry.sol updated with enum and struct and state variables

--- src/ProjectRegistry.sol
+++ src/ProjectRegistry.sol
@@ -2,4 +2,36 @@
 
 pragma solidity ^0.8.19;
 
-contract ProjectRegistry {}
+contract ProjectRegistry {
+
+    enum ProjectReturnType{
+        Payback,
+        Interest,
+        Nothing
+    }
+
+    enum ProjectStatus {
+        Voting,
+        Approved,
+        Rejected,
+        FundRaising,
+        OnGoing,
+        Completed
+    }
+
+    struct Project {
+        uint256 projectId;
+        address projectOwner;
+        string projectName;
+        string projectCategory;
+        string projectDescription;
+        uint256 createdAt;
+        uint256 fundingGoal;
+        string projectObjectives;
+        ProjectReturnType projectReturnType;
+        ProjectStatus projectStatus;
+    }
+
+    uint256 private totalProjects;
+    mapping (uint256 => Project) private projectIdToProject;
+}
