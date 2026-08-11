fe2a95328c0072ff8f5117830ba595614a3bb585
aabhushan <aabhushans62@gmail.com>
2026-08-11 16:33:14 +0545

    created ProjectRegistryTests.t.sol

---
+++ test/ProjectRegistryTests.t..sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Test} from "../lib/forge-std/src/Test.sol";
+import {ProjectRegistry} from "../src/ProjectRegistry.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ProjectRegistryTests is Test {}
