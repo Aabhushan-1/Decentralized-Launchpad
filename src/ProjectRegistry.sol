d7b2cdb76ba8198907685f011d0080cda7f37f10
aabhushan <aabhushans62@gmail.com>
2026-08-11 16:04:14 +0545

    submitProject() added

--- src/ProjectRegistry.sol
+++ src/ProjectRegistry.sol
@@ -38,14 +38,32 @@ contract ProjectRegistry {
     event ProjectApproved(uint256 indexed projectId);
     event ProjectRejected(uint256 indexed projectId);
 
-    function submitProject(string memory _projectName, string memory _projectCategory, string memory _projectDescription, uint256 _fundingGoal, string memory _projectObjectives, ProjectReturnType _projectReturnType) public {
+    function submitProject(
+        string memory _projectName,
+        string memory _projectCategory,
+        string memory _projectDescription,
+        uint256 _fundingGoal,
+        string memory _projectObjectives,
+        ProjectReturnType _projectReturnType
+    ) public {
         uint256 _projectId = totalProjects;
         address _projectOwner = msg.sender;
         uint256 _createdAt = block.timestamp;
         ProjectStatus _projectStatus = ProjectStatus.Voting;
 
-        projectIdToProject[_projectId] = Project(_projectId, _projectOwner, _projectName, _projectCategory, _projectDescription, _createdAt, _fundingGoal, _projectObjectives, _projectReturnType, _projectStatus);
-        totalProjects ++;
+        projectIdToProject[_projectId] = Project(
+            _projectId,
+            _projectOwner,
+            _projectName,
+            _projectCategory,
+            _projectDescription,
+            _createdAt,
+            _fundingGoal,
+            _projectObjectives,
+            _projectReturnType,
+            _projectStatus
+        );
+        totalProjects++;
 
         emit ProjectSubmitted(_projectId);
     }
