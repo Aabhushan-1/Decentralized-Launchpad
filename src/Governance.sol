9c569c44a77b7f52a026fba0d145792d24a0d157
aabhushan <aabhushans62@gmail.com>
2026-08-12 09:22:21 +0545

    state variables added

--- src/Governance.sol
+++ src/Governance.sol
@@ -2,4 +2,40 @@
 
 pragma solidity ^0.8.19;
 
-contract Governance {}
+import {ProjectRegistry} from "../src/ProjectRegistry.sol";
+
+contract Governance {
+    enum SessionStatus {
+        ongoing,
+        ended
+    }
+
+    struct Proposal {
+        uint256 projectId;
+        uint256 votes;
+    }
+
+    struct VotingSession {
+        uint256 sessionId;
+        uint256 duration;
+        uint256 deadline;
+        bool executed;
+        Proposal[] proposals;
+        SessionStatus sessionStatus;
+    }
+
+    ProjectRegistry private projectRegistry;
+
+    constructor(address _projectRegistry) {
+        projectRegistry = ProjectRegistry(_projectRegistry);
+    }
+
+    uint256 private totalSessions;
+    uint256 private constant QUORUM = 100;
+    mapping(uint256 => VotingSession) private sessions;
+    mapping(address => mapping(uint256 => bool)) private addressHasVoted;
+
+    event SessionCreated(uint256 indexed sessionId);
+    event Voted(uint256 indexed sessionId, uint256 indexed projectId, address voter);
+    event SessionExecuted(uint256 indexed sessionId, uint256 winningProjectId);
+}
