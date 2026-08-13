3004953aa62678187e3697bb3f2f3f84f6edb779
aabhushan <aabhushans62@gmail.com>
2026-08-13 07:01:55 +0545

    GovernanceTests setUp() added

--- test/GovernanceTests.t.sol
+++ test/GovernanceTests.t.sol
@@ -3,6 +3,14 @@
 pragma solidity ^0.8.19;
 
 import {Governance} from "../src/Governance.sol";
+import {ProjectRegistry} from "../src/ProjectRegistry.sol";
 import {Test} from "../lib/forge-std/src/Test.sol";
 
-contract GovernanceTests is Test {}
+contract GovernanceTests is Test {
+    Governance governance;
+    ProjectRegistry projectRegistry;
+
+    function setUp() public {
+        governance = new Governance(address(projectRegistry));
+    }
+}
