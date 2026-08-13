9ec89eb60ec0f9d7957b0bafda5d72d1d338fecf
aabhushan <aabhushans62@gmail.com>
2026-08-13 05:55:01 +0545

    GovernanceTests.t.sol added

---
+++ test/GovernanceTests.t.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identidier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Governance} from "../src/Governance.sol";
+import {Test} from "../lib/forge-std/src/Test.sol";
+
+contract GovernanceTests is Test {}
