0a810224c61acc1cf9d1b87ad1e2481e081f2209
aabhushan <aabhushans62@gmail.com>
2026-08-16 15:15:58 +0545

    FundingRoundTests.t.sol added

---
+++ test/FundingRoundTests.t.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {ProjectRegistry} from "../src/ProjectRegistry.sol";
+import {FundingRound} from "../src/FundingRound.sol";
+import {Test} from "../lib/forge-std/src/Test.sol";
+
+contract FundingRoundTests is Test {}
